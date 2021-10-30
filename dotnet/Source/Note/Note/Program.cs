fd410b2b28ef6cd57ac34b254df8531cb74d1e5a
np90s <mr.phucnguyen@hotmail.com>
2021-10-30 16:40:50 +0700

    Added Extension Method, Chaining Method

--- dotnet/Source/Note/Note/Program.cs
+++ dotnet/Source/Note/Note/Program.cs
@@ -1,12 +1,31 @@
 ﻿using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
 
 namespace Note
 {
-    class Program
+    static class ExtensionClass
     {
-        static void Main(string[] args)
+        public static void Print(this string s, ConsoleColor color)
         {
-            Console.WriteLine("Hello World!");
+            Console.ForegroundColor = color;
+            Console.WriteLine(s);
+        }
+    }
+
+    public class JsonTemplateBuilder
+    {
+        private Dictionary<string, string> _options = new Dictionary<string, string>();
+
+        public JsonTemplateBuilder Add(string key, string value)
+        {
+            if (!_options.ContainsKey(key))
+                _options.Add(key, value);
+            return this;
+        }
+        public string Build()
+        {
+            return $"{{{JsonConvert.SerializeObject(_options)}}}";
         }
     }
 }

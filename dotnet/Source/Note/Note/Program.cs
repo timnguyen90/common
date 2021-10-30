061740e6b820a1f41251a374af96c665d5213e4a
np90s <mr.phucnguyen@hotmail.com>
2021-10-30 11:22:04 +0700

    Added the origin source main of the note

---
+++ dotnet/Source/Note/Note/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Note
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

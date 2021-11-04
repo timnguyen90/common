a74b908b7ead569b0f166ce36c4a339a60a27e72
DESKTOP-IDO4IVP\Tim <phucnv90@gmail.com>
2021-11-04 14:30:13 +0700

    Added database models, migrations, seeding data.

---
+++ dotnet/Source/RespositoryPattern/Entities/Configuration/CompanyConfiguration.cs
@@ -0,0 +1,25 @@
+﻿using Entities.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+
+namespace Entities.Configuration
+{
+    public class CompanyConfiguration : IEntityTypeConfiguration<Company>
+    {
+        public void Configure(EntityTypeBuilder<Company> builder)
+        {
+            builder.HasData(
+                new Company { 
+                    Id = new Guid("c9d4c053-49b6-410c-bc78-2d54a9991870"), 
+                    Name = "Company 1", 
+                    Address = "Street 1",
+                    Country = "USA" }, 
+                new Company { 
+                    Id = new Guid("3d490a70-94ce-4d15-9494-5248280c2ce3"), 
+                    Name = "Company 2", 
+                    Address = "Street 2", 
+                    Country = "VN" });
+        }
+    }
+}
---
+++ dotnet/Source/RespositoryPattern/Entities/Extensions/ServiceExtension.cs
@@ -0,0 +1,16 @@
+﻿using Entities.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Entities.Extensions
+{
+    public static class ServiceExtension
+    {
+        public static void ConfigureSqlContext(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<RepositoryContext>(opts => 
+            opts.UseSqlServer(configuration.GetConnectionString("sqlConnection"),b => b.MigrationsAssembly("RespositoryPattern")));
+        }
+    }
+}

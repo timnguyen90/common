44374b9bfa4c67bbcffbdcc2cb819af6c656e7fd
Tim Nguyen <mr.phucnguyen@hotmail.com>
2021-11-09 09:27:25 +0700

    Added Create company with a list of employees.

--- dotnet/Source/RespositoryPattern/Entities/DTO/CompanyForCreationDto.cs
+++ dotnet/Source/RespositoryPattern/Entities/DTO/CompanyForCreationDto.cs
@@ -1,9 +1,12 @@
-﻿namespace Entities.DTO
+﻿using System.Collections.Generic;
+
+namespace Entities.DTO
 {
     public class CompanyForCreationDto
     {
         public string Name { get; set; }
         public string Address { get; set; }
         public string Country { get; set; }
+        public IEnumerable<EmployeeForCreationDto> Employees { get; set; }
     }
 }

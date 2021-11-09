7747ceb60abaa990018c66a790e2ac9aab668d7d
Tim Nguyen <mr.phucnguyen@hotmail.com>
2021-11-10 02:11:54 +0700

    Added Update Employee.

---
+++ dotnet/Source/RespositoryPattern/Entities/DTO/EmployeeForUpdateDto.cs
@@ -0,0 +1,9 @@
+﻿namespace Entities.DTO
+{
+    public class EmployeeForUpdateDto
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Position { get; set; }
+    }
+}

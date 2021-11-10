5ca3e227185c9c224a482761a50bb06bb76a8cfe
Tim Nguyen <mr.phucnguyen@hotmail.com>
2021-11-11 00:52:49 +0700

    Added Validation for all request types.

--- dotnet/Source/RespositoryPattern/Entities/DTO/EmployeeForUpdateDto.cs
+++ dotnet/Source/RespositoryPattern/Entities/DTO/EmployeeForUpdateDto.cs
@@ -1,9 +1,6 @@
 ﻿namespace Entities.DTO
 {
-    public class EmployeeForUpdateDto
+    public class EmployeeForUpdateDto: EmployeeForManipulationDto
     {
-        public string Name { get; set; }
-        public int Age { get; set; }
-        public string Position { get; set; }
     }
 }
--- dotnet/Source/RespositoryPattern/RespositoryPattern/Startup.cs
+++ dotnet/Source/RespositoryPattern/RespositoryPattern/Startup.cs
@@ -4,6 +4,7 @@ using Logging;
 using Logging.Interfaces;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -29,7 +30,12 @@ namespace RespositoryPattern
             services.ConfigureRepositoryManager();
             services.ConfigureLoggerService();
             services.AddAutoMapper(typeof(Startup));
-            
+
+            services.Configure<ApiBehaviorOptions>(options =>
+            {
+                options.SuppressModelStateInvalidFilter = true;
+            });
+
             services.AddControllers(config =>
             {
                 config.RespectBrowserAcceptHeader = true;

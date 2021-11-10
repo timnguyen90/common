803a3898d1ed6fb5e4eacf9d182757e576fde66b
Tim Nguyen <mr.phucnguyen@hotmail.com>
2021-11-10 15:27:08 +0700

    Added PATCH to the employee

--- dotnet/Source/RespositoryPattern/RespositoryPattern/Controllers/EmployeesController.cs
+++ dotnet/Source/RespositoryPattern/RespositoryPattern/Controllers/EmployeesController.cs
@@ -6,6 +6,7 @@ using Entities.DTO;
 using Entities.Repositories;
 using Logging.Interfaces;
 using Entities.Models;
+using Microsoft.AspNetCore.JsonPatch;
 
 namespace RespositoryPattern.Controllers
 {
@@ -133,6 +134,39 @@ namespace RespositoryPattern.Controllers
             _mapper.Map(employee, employeeEntity);
             _repository.Save();
 
+            return NoContent();
+        }
+
+        [HttpPatch("{id}")]
+        public IActionResult PartiallyUpdateEmployeeForCompany(
+            Guid companyId, 
+            Guid id, 
+            [FromBody] JsonPatchDocument<EmployeeForUpdateDto> patchDoc)
+        {
+            if(patchDoc == null)
+            {
+                _logger.LogError("patchDoc object sent from client is null.");
+                return BadRequest("patchDoc object is null");
+            }
+
+            var company = _repository.Company.GetCompany(companyId, trackChanges: false);
+            if(company == null)
+            {
+                _logger.LogInfo($"Company with id: {companyId} doesn't exist in the database.");
+                return NotFound();
+            }
+
+            var employeeEntity = _repository.Employee.GetEmployee(companyId, id, trackChanges: true);
+            if(employeeEntity == null)
+            {
+                _logger.LogInfo($"Employee with id: {id} doesn't exist in the database.");
+                return NotFound();
+            }
+
+            var employeeToPatch = _mapper.Map<EmployeeForUpdateDto>(employeeEntity);
+            patchDoc.ApplyTo(employeeToPatch);
+            _mapper.Map(employeeToPatch, employeeEntity);
+            _repository.Save();
             return NoContent();
         }
     }
